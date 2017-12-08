18b336e80fd3449482edd9dbe8a287d13ff14fed
Alexandre van Beurden <>
2017-12-08 21:54:14 +0100

    Pole doctrine test

---
+++ shareticREST/src/ShareticBundle/Entity/Pole.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace ShareticBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pole
+ *
+ * @ORM\Table(name="pole")
+ * @ORM\Entity(repositoryClass="ShareticBundle\Repository\PoleRepository")
+ */
+class Pole
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=25, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Pole
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Pole
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
