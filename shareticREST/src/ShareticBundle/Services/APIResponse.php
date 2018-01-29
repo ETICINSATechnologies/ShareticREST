b3f45b2aacdc93c67864b6a5451714035c50a6a1
Alexandre van Beurden <>
2018-01-29 20:00:07 +0100

    Add header field

--- shareticREST/src/ShareticBundle/Services/APIResponse.php
+++ shareticREST/src/ShareticBundle/Services/APIResponse.php
@@ -4,19 +4,33 @@
 
 namespace ShareticBundle\Services;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 class APIResponse
 {
+    private function jsonResponse($array){
+        $response = new Response();
+
+        $response->setContent(json_encode($array));
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $response;
+
+    }
     public function returnResponse($array){
         $return = array();
         $return['success']=true;
         $return['res']=$array;
-        return new JsonResponse($return);
+
+
+        return $this->jsonResponse($return);
     }
     public function returnError($code){
         $return = array();
         $return['success']=false;
         $return['code']=$code;
-        return new JsonResponse($return);
+
+        return $this->jsonResponse($return);
     }
 }
