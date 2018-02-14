f2532617ccf5c48503969c0fcba54d60fe1e5406
Alexandre van Beurden <>
2018-02-14 21:07:42 +0100

    Add endpoints

--- shareticREST/src/ShareticBundle/Controller/ChapterController.php
+++ shareticREST/src/ShareticBundle/Controller/ChapterController.php
@@ -13,14 +13,30 @@ class ChapterController extends Controller
 
     public function getChapter($idChapter)
     {
-        //Initializing the service
         $APIResp = $this->container->get('sharetic.APIResponse');
+        $entityFormatter = $this->container->get('sharetic.EntityFormatter');
 
+        $em = $this->getDoctrine()->getManager();
+        $chapter = $em->getRepository('ShareticBundle:Chapter')->find($idChapter);
+
+        if($chapter === null){
+            return $APIResp->returnError("C_NF");
+        }
+
+        $files = $em->getRepository('ShareticBundle:File')->findBy(array("chapter"=>$chapter));
+        $fileList = array();
+
+        foreach ($files as $file) {
+            $filesRes=$entityFormatter->formatFile($file);
+            $filesRes['draft']=$file->getIsDraft();
+            array_push($fileList,$filesRes);
+        }
         //Just an example of a possible structure of the response
-        $response = array();
-        $response[0]="titre du chapitre";
-        $response[1]="texte du chapitre";
-
+        $response = $entityFormatter->formatChapter($chapter);
+        $response['draft']=$chapter->getIsDraft();
+        $response['content']=$chapter->getContent();
+        $response['files']=$fileList;
+        $response['formation']=$entityFormatter->formatFormation($chapter->getFormation());
 
         return $APIResp->returnResponse($response);
     }
